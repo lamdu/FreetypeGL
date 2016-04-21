77c192c6183ddbbd95e07ac99aa7d516b1f2e281
Eyal Lotem <eyal.lotem@gmail.com>
2016-04-21 13:26:36 +0300

    Use Maybe Colors instead of Bool, Color

--- src/Graphics/FreetypeGL/Markup.hs
+++ src/Graphics/FreetypeGL/Markup.hs
@@ -8,6 +8,10 @@ module Graphics.FreetypeGL.Markup
 
 import qualified Bindings.FreetypeGL.Markup as MU
 import           Bindings.FreetypeGL.Vec234 (C'vec4(..))
+import           Data.Function ((&))
+import           Data.Maybe (fromMaybe)
+import           Data.Maybe (isJust)
+import           Foreign.C.Types (CInt)
 import           Foreign.Marshal.Alloc (alloca)
 import           Foreign.Ptr (Ptr, nullPtr)
 import           Foreign.Storable (Storable(..))
@@ -27,14 +31,10 @@ data Markup = Markup
     , gamma :: !Float
     , foregroundColor :: !RGBA
     , backgroundColor :: !RGBA
-    , outline :: !Bool
-    , outlineColor :: !RGBA
-    , underline :: !Bool
-    , underlineColor :: !RGBA
-    , overline :: !Bool
-    , overlineColor :: !RGBA
-    , strikethrough :: !Bool
-    , strikethroughColor :: !RGBA
+    , outlineColor :: !(Maybe RGBA)
+    , underlineColor :: !(Maybe RGBA)
+    , overlineColor :: !(Maybe RGBA)
+    , strikethroughColor :: !(Maybe RGBA)
     }
 
 def :: Markup
@@ -44,18 +44,16 @@ def =
     , gamma = 1.0
     , foregroundColor = white
     , backgroundColor = noColor
-    , outline = False
-    , outlineColor = noColor
-    , underline = False
-    , underlineColor = noColor
-    , overline = False
-    , overlineColor = noColor
-    , strikethrough = False
-    , strikethroughColor = noColor
+    , outlineColor = Nothing
+    , underlineColor = Nothing
+    , overlineColor = Nothing
+    , strikethroughColor = Nothing
     }
     where
         white = RGBA 1.0 1.0 1.0 1.0
-        noColor = RGBA 0.0 0.0 0.0 0.0
+
+noColor :: RGBA
+noColor = RGBA 0.0 0.0 0.0 0.0
 
 rgbaToVec4 :: RGBA -> C'vec4
 rgbaToVec4 (RGBA r g b a) =
@@ -79,16 +77,16 @@ withMarkupPtr Markup{..} font@(TextureFont fontPtr) act =
                 (realToFrac gamma)
                 (rgbaToVec4 foregroundColor)
                 (rgbaToVec4 backgroundColor)
-                (asCBool outline)
-                (rgbaToVec4 outlineColor)
-                (asCBool underline)
-                (rgbaToVec4 underlineColor)
-                (asCBool overline)
-                (rgbaToVec4 overlineColor)
-                (asCBool strikethrough)
-                (rgbaToVec4 strikethroughColor)
-                fontPtr
+                & passColorAsCParams outlineColor
+                & passColorAsCParams underlineColor
+                & passColorAsCParams overlineColor
+                & passColorAsCParams strikethroughColor
+                & ($ fontPtr)
         poke ptr cMarkup
         act ptr
-    where
-        asCBool = fromIntegral . fromEnum
+
+passColorAsCParams :: Maybe RGBA -> (CInt -> C'vec4 -> a) -> a
+passColorAsCParams mColor f =
+    f
+    ((fromIntegral . fromEnum . isJust) mColor)
+    (rgbaToVec4 (fromMaybe noColor mColor))
